3b9ad06a6b0810418b7c824b8b82e68ddca37256
joshuasparkes <joshsparkes6@gmail.com>
2025-06-13 22:11:25 +0100

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -4,31 +4,14 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <p className="mb-2 text-4xl tracking-[-.01em]">Future Portfolio Site</p>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Provide your designs
-          </li>
-          <li className="mb-2 tracking-[-.01em]">
-            And content
-          </li>
-          <li className="mb-2 tracking-[-.01em]">
-            And preferred url
-          </li>
+          <li className="mb-2 tracking-[-.01em]">Provide your designs</li>
+          <li className="mb-2 tracking-[-.01em]">And content</li>
+          <li className="mb-2 tracking-[-.01em]">And preferred url</li>
         </ol>
-
-        
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
